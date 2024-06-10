1fa77bb03391ed2fbf129ba39f4e39010acb4c11
Dipti-Deshmane <itsmedipu17@gmail.com>
2024-06-10 17:40:21 +0530

    UC2 added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -12,8 +12,35 @@ function checkEmployeeStatus(){
     return status;
 }
 
+function calculateDailyWage(){
+    const workType = Math.floor(Math.random()*3); //to calculate no time, part time and full time
+    const wagePerHour = 20;
+
+    let hoursWorked;
+
+    switch(workType){
+        case 0: hoursWorked= 0;
+                break;
+        case 1: hoursWorked= 4;
+                break;
+        case 2: hoursWorked= 8;
+                break;
+        default: hoursWorked=0;
+     }
+
+     const dailyWage = hoursWorked*wagePerHour;
+     return dailyWage;
+}
+
 
 const employeeStatus = checkEmployeeStatus();
 console.log(`Employee is ${employeeStatus}`);
+
+if (employeeStatus === "Present") {
+    const dailyWage = calculateDailyWage();
+    console.log(`Employee's daily wage is ${dailyWage}`);
+} else {
+    console.log("Employee's daily wage is 0");
+}
 
 
